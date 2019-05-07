bdb7dd1e50894a3e6b8ee3ce294c738c7940e287
Nick Ballenger <nballeng@gmail.com>
2019-05-07 15:01:07 -0700

    revision to env vars for versions

--- setup-local.sh
+++ setup-local.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
+SCRIPT_NAME=$0
 
 # This file sets environment variables useful for building / using this
 # repo. Note that to use it you should run `source setup-env.sh`, since
 # if you just run the file directly it will spawn its own process
 # instead of adding to the environment of the current shell.
-
-# COMPOSE_PROJECT_NAME is a docker-compose env var, and determines the prefix
-# that will be used when naming containers created via docker-compose. 
-# See https://docs.docker.com/compose/reference/envvars/ for more info.
-export COMPOSE_PROJECT_NAME="carto-ms"
 
 # These are used to git checkout each submodule to specific tags. Once the
 # submodules are checked out to version tags, your docker-compose builds will
@@ -23,20 +19,53 @@ CARTO_WINDSHAFT_SUBMODULE_PATH="./docker/windshaft/Windshaft-cartodb"
 CARTO_CARTODB_SUBMODULE_PATH="./docker/cartodb/cartodb"
 CARTO_SQLAPI_SUBMODULE_PATH="./docker/sqlapi/CartoDB-SQL-API"
 
-SET_CHECKOUTS=yes
+SET_CHECKOUTS=no
 QUIET=no
 GITQUIET=""
+HORIZONTAL_LINE="\n$(printf '=%.0s' {1..79})\n\n"
 
-# TODO: Add help text for the script.
-while test $# -gt 0; do
+function display_help() {
+    local help_text=""
+    IFS='' read -r -d '' help_text <<EOF
+
+Usage: $SCRIPT_NAME [--set-submodule-versions] [-q|--quiet]
+
+Purpose: Sets the following environment variables (current value in parens):
+
+    CARTO_PGEXT_VERSION       ($CARTO_PGEXT_VERSION)
+    CARTO_WINDSHAFT_VERSION   ($CARTO_WINDSHAFT_VERSION)
+    CARTO_CARTODB_VERSION     ($CARTO_CARTODB_VERSION)
+    CARTO_SQLAPI_VERSION      ($CARTO_SQLAPI_VERSION)
+
+    If the --set-submodule-versions flag is present, resets the
+    submodule directories to the version tags in those variables.
+
+    Note that the values are set both in the local environment, and
+    in the .env file that docker-compose uses to substitute environment
+    variables. That file is excluded from version control, so you
+    have to run this script at least once with --set-submodule-versions
+    before attempting docker-compose build.
+
+Flags:
+    --set-submodule-versions   - For all submodules in the project,
+                                 pull from master, then re-checkout
+                                 to the version tag listed in the script.
+    -q|--quiet                 - Display no output.
+
+EOF
+
+    printf "$help_text"
+}
+
+while [[ $# -gt 0 ]]; do
     case "$1" in
         -h|--help)
-            printf "Help for this script."
+            display_help
             exit 0
             ;;
-        --no-set-submodule-versions)
+        --set-submodule-versions)
             shift
-            SET_CHECKOUTS=no
+            SET_CHECKOUTS=yes
             ;;
         -q|--quiet)
             shift
@@ -55,32 +84,72 @@ function echo_if_unquiet() {
     fi
 }
 
-echo_if_unquiet "\nCurrent version strings for Carto submodules:\n"
-echo_if_unquiet "    Carto PostgreSQL Extension: $CARTO_PGEXT_VERSION\n"
-echo_if_unquiet "    Carto Windshaft:            $CARTO_WINDSHAFT_VERSION\n"
-echo_if_unquiet "    Carto SQLAPI:               $CARTO_SQLAPI_VERSION\n"
-echo_if_unquiet "    CartoDB:                    $CARTO_CARTODB_VERSION\n\n"
+IFS='' read -r -d '' vstrings <<EOF
 
-if [ "$SET_CHECKOUTS" = "yes" ]; then
+Current version strings for Carto submodules:
+    Carto PostgreSQL Extension: $CARTO_PGEXT_VERSION
+    Carto Windshaft:            $CARTO_WINDSHAFT_VERSION
+    Carto SQLAPI:               $CARTO_SQLAPI_VERSION
+    CartoDB:                    $CARTO_CARTODB_VERSION
+
+EOF
+
+echo_if_unquiet "$vstrings"
+
+if [[ "$SET_CHECKOUTS" = "yes" ]]; then
+    # Need to set the values in the .env file, so docker-compose merge values
+    # for individual containers work.
+    echo "CARTO_PGEXT_VERSION=$CARTO_PGEXT_VERSION" > ./.env
+    echo "CARTO_WINDSHAFT_VERSION=$CARTO_WINDSHAFT_VERSION" >> ./.env
+    echo "CARTO_SQLAPI_VERSION=$CARTO_SQLAPI_VERSION" >> ./.env
+    echo "CARTO_CARTODB_VERSION=$CARTO_CARTODB_VERSION" >> ./.env
+
+    # Going to turn off warnings about detached head, but should be able to
+    # set it back to the local value if there is one at the end of the script.
+    CURRENT_DETACHED_HEAD_ADVICE=$(git config --global --get advice.detachedHead)
+    git config --global advice.detachedHead false
+
     echo_if_unquiet "Setting checkouts to current version strings...\n"
 
-    echo_if_unquiet "    Setting $CARTO_PGEXT_SUBMODULE_PATH to version $CARTO_PGEXT_VERSION... "
+    echo_if_unquiet "$HORIZONTAL_LINE"
+    echo_if_unquiet "Carto PostgreSQL Extension\n\n"
+    echo_if_unquiet "Checking out tag '$CARTO_PGEXT_VERSION' in $CARTO_PGEXT_SUBMODULE_PATH:\n\n"
+    set -x
     git --git-dir=$CARTO_PGEXT_SUBMODULE_PATH/.git checkout $GITQUIET master
     git --git-dir=$CARTO_PGEXT_SUBMODULE_PATH/.git pull $GITQUIET
     git --git-dir=$CARTO_PGEXT_SUBMODULE_PATH/.git checkout $GITQUIET $CARTO_PGEXT_VERSION
+    { set +x; } 2>/dev/null
 
-    echo_if_unquiet "    Setting $CARTO_WINDSHAFT_SUBMODULE_PATH to version $CARTO_WINDSHAFT_VERSION... "
+    echo_if_unquiet "$HORIZONTAL_LINE"
+    echo_if_unquiet "Carto Windshaft\n\n"
+    echo_if_unquiet "Checking out tag '$CARTO_WINDSHAFT_VERSION' in $CARTO_WINDSHAFT_SUBMODULE_PATH:\n\n"
+    set -x
     git --git-dir=$CARTO_WINDSHAFT_SUBMODULE_PATH/.git checkout $GITQUIET master
     git --git-dir=$CARTO_WINDSHAFT_SUBMODULE_PATH/.git pull $GITQUIET
     git --git-dir=$CARTO_WINDSHAFT_SUBMODULE_PATH/.git checkout $GITQUIET $CARTO_WINDSHAFT_VERSION
+    { set +x; } 2>/dev/null
 
-    echo_if_unquiet "    Setting $CARTO_SQLAPI_SUBMODULE_PATH to version $CARTO_SQLAPI_VERSION... "
+    echo_if_unquiet "$HORIZONTAL_LINE"
+    echo_if_unquiet "Carto SQL API\n\n"
+    echo_if_unquiet "Checking out tag '$CARTO_SQLAPI_VERSION' in $CARTO_SQLAPI_SUBMODULE_PATH:\n\n"
+    set -x
     git --git-dir=$CARTO_SQLAPI_SUBMODULE_PATH/.git checkout $GITQUIET master
     git --git-dir=$CARTO_SQLAPI_SUBMODULE_PATH/.git pull $GITQUIET
     git --git-dir=$CARTO_SQLAPI_SUBMODULE_PATH/.git checkout $GITQUIET $CARTO_SQLAPI_VERSION
+    { set +x; } 2>/dev/null
 
-    echo_if_unquiet "    Setting $CARTO_CARTODB_SUBMODULE_PATH to version $CARTO_CARTODB_VERSION... "
+    echo_if_unquiet "$HORIZONTAL_LINE"
+    echo_if_unquiet "CartoDB\n\n"
+    echo_if_unquiet "Checking out tag '$CARTO_CARTODB_VERSION' in $CARTO_CARTODB_SUBMODULE_PATH:\n\n"
+    set -x
     git --git-dir=$CARTO_CARTODB_SUBMODULE_PATH/.git checkout $GITQUIET master
     git --git-dir=$CARTO_CARTODB_SUBMODULE_PATH/.git pull $GITQUIET
     git --git-dir=$CARTO_CARTODB_SUBMODULE_PATH/.git checkout $GITQUIET $CARTO_CARTODB_VERSION
+    { set +x; } 2>/dev/null
+
+    if [[ -n $CURRENT_DETACHED_HEAD_ADVICE ]]; then
+        git config --global advice.detachedHead "$CURRENT_DETACHED_HEAD_ADVICE"
+    else
+        git config --global --unset advice.detachedHead
+    fi
 fi
