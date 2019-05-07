e2b8e84f86a930aeb09cb433d07cb19754e7c538
Nick Ballenger <nballeng@gmail.com>
2019-05-07 15:01:44 -0700

    revisions to container start, logging

--- docker/cartodb/docker-entrypoint.sh
+++ docker/cartodb/docker-entrypoint.sh
@@ -7,4 +7,5 @@ echo "Updating config files with current IP of container..."
 sed -i "s/127.0.0.1/$(hostname -i)/g" ./config/*.yml
 echo "Starting the rails server process on $(hostname -i):3000..."
 bundle exec rails server -b $(hostname -i) -d
-bundle exec ./script/resque
+bundle exec ./script/resque > /dev/null 2>&1
+tail -f /dev/null
