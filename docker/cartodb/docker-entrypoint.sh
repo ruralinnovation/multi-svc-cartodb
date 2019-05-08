0b65ff18b26b6cce22e60574f684598e021214db
Nick Ballenger <nballeng@gmail.com>
2019-05-07 19:34:34 -0700

    working on entrypoint and create dev user

--- docker/cartodb/docker-entrypoint.sh
+++ docker/cartodb/docker-entrypoint.sh
@@ -6,7 +6,26 @@ DEFAULT_USER=${CARTO_DEFAULT_USER:-developer}
 PASSWORD=${CARTO_DEFAULT_PASS:-dev123}
 EMAIL=${CARTO_DEFAULT_EMAIL:-username@example.com}
 
-PUBLIC_HOST=${PUBLIC_HOST:-localhost}
-PUBLIC_PORT=${PUBLIC_PORT:-3000}
+dev_db_name=$(sed -n '/development:/,/test:/p;/test:/q' config/database.yml \
+              | grep 'database:' \
+              | sed 's/database://' \
+              | sed -e 's/[[:space:]]//g')
+
+dev_db_exists=$(psql -U postgres -h postgis -lqt \
+                | cut -d \| -f 1 \
+                | grep "$dev_db_name")
+
+if [[ -n dev_db_exists ]]; then
+    echo "Database $dev_db_name already exists in postgres, skipping db:create rake task"
+    echo "Running database migrations, with silenced output."
+    bundle exec rake db:migrate > /dev/null 2>&1
+else
+    echo "Creating database $dev_db_name..."
+    bundle exec rake db:create
+    echo "Migrating database $dev_db_name..."
+    bundle exec rake db:migrate
+fi
+
+
 
 tail -f /dev/null
