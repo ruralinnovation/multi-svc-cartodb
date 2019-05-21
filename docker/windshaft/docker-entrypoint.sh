889a1013913580f971e1b46cb61187e3bc28e550
Nick Ballenger <nballeng@gmail.com>
2019-05-20 17:18:29 -0700

    Removing keepalive instruction for windshaft

--- docker/windshaft/docker-entrypoint.sh
+++ docker/windshaft/docker-entrypoint.sh
@@ -4,5 +4,3 @@ set -e
 
 echo "Starting the Windshaft node application..."
 exec node app -c config/environments/development.js
-#echo "Tailing the windshaft logs"
-#tail -f /carto/Windshaft-cartodb/logs/node-windshaft.log
