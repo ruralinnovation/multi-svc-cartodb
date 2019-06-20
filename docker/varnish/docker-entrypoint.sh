a8f70764a16afcf524f3885d2add577772dc429d
Nick Ballenger <nballeng@gmail.com>
2019-06-20 14:20:38 -0700

    Removing -S flag from varnish startup
    Turns out that using the secret prevents telnet connections.

--- docker/varnish/docker-entrypoint.sh
+++ docker/varnish/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-VARNISH_SECRET=${VARNISH_SECRET:-abc123def}
-
-mkdir -p /etc/varnish
-echo "$VARNISH_SECRET" > /etc/varnish/secret
 echo "Starting varnish daemon..."
-/opt/varnish/sbin/varnishd -a :6081 -T :6082 -s malloc,256m -f /etc/varnish.vcl -S /etc/varnish/secret
+/opt/varnish/sbin/varnishd -a :6081 -T :6082 -s malloc,256m -f /etc/varnish.vcl
 echo "Tailing /dev/null to keep the Docker image from closing."
 tail -f /dev/null
