01913e52d5a0ebfdb3bbffe2d4de5ba7fc3fd498
Nick Ballenger <nballeng@gmail.com>
2019-07-26 11:19:26 -0700

    Remove telnet, pipe logs to compose

--- docker/varnish/docker-entrypoint.sh
+++ docker/varnish/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting varnish daemon..."
-/opt/varnish/sbin/varnishd -a :6081 -T :6082 -s malloc,256m -f /etc/varnish.vcl
-echo "Tailing /dev/null to keep the Docker image from closing."
-tail -f /dev/null
+/opt/varnish/sbin/varnishd -a :6081 -s malloc,256m -f /etc/varnish.vcl
+echo "Starting varnishlog -c to view client requests"
+/opt/varnish/bin/varnishlog -c
