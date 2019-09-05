16725f164c96725e370590152a6423e678bd6d46
Nick Ballenger <nballeng@gmail.com>
2019-09-05 11:03:52 -0700

    Picking up various changes to docker ctx

--- docker/sqlapi/entrypoint.sh
+++ docker/sqlapi/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+CONFIG_FILE="config/environments/${CARTO_ENV}.js"
 set -e
 
 echo "Starting the SQLAPI node application..."
-exec node app -c config/environments/development.js
+exec node app -c $CONFIG_FILE
--- docker/varnish/entrypoint.sh
+++ docker/varnish/entrypoint.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-echo "Starting varnish daemon..."
-/opt/varnish/sbin/varnishd -a :6081 -s malloc,256m -f /etc/varnish.vcl
-echo "Starting varnishlog -c to view client requests"
-/opt/varnish/bin/varnishlog -c
+#### SETTINGS FROM ENVIRONMENT ###############################################
+
+VARNISH_HTTP_PORT=${VARNISH_HTTP_PORT}
+
+REQUIRED_ENV_VARS=(VARNISH_HTTP_PORT)
+
+REQS_MET="yes"
+for var in ${REQUIRED_ENV_VARS[@]}; do
+    if [[ -z ${!var} ]]; then
+        echo "CRITICAL: In script ${0}, ${var} not found in environment."
+        REQS_MET="no"
+    fi
+done
+
+if [[ $REQS_MET != "yes" ]]; then
+    echo "${0} exiting, insufficient info from env."; exit 1
+fi
+
+#### ENTRYPOINT TASKS ########################################################
+
+echo "Starting varnishd (foregrounded) as container entrypoint."
+/opt/varnish/sbin/varnishd -F \
+    -a :${VARNISH_HTTP_PORT} \
+    -s malloc,256m \
+    -f /etc/varnish.vcl
