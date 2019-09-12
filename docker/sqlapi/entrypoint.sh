e874143437ffe407717e4f12b8f9eb91de7a9c4a
Nick Ballenger <nballeng@gmail.com>
2019-09-12 12:01:28 -0400

    Upped sqlapi node max http header size

--- docker/sqlapi/entrypoint.sh
+++ docker/sqlapi/entrypoint.sh
@@ -4,4 +4,4 @@ CONFIG_FILE="config/environments/${CARTO_ENV}.js"
 set -e
 
 echo "Starting the SQLAPI node application..."
-exec node app -c $CONFIG_FILE
+exec node app -c $CONFIG_FILE --max-http-header-size=65536
